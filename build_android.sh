caa3bb389a15c3416bd1fe70d836a1794a895a32
aaccurso <accurso.alan@gmail.com>
2014-10-17 15:46:24 -0300

    refactors build android script

--- build_android.sh
+++ build_android.sh
@@ -1,5 +1,9 @@
-BUILD_HOME=$PWD/dist
+#!/bin/sh
+PROJECT_HOME=$PWD
+DIST_HOME=$PROJECT_HOME/dist
+BUILD_HOME=$PROJECT_HOME/build
+
 cd $DEV_HOME/crosswalk-8.37.189.12 &&
-python make_apk.py --package $1 --manifest $BUILD_HOME/manifest.json &&
+python make_apk.py --package $1 --manifest $DIST_HOME/manifest.json &&
 rm -fr $BUILD_HOME/$2 && mv -f $2 $BUILD_HOME && mv -f $2_arm.apk $BUILD_HOME && rm -f $2_x86.apk
-cd $BUILD_HOME
+cd $PROJECT_HOME
