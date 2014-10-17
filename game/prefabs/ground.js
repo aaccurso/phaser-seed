d36c9d1bad147ddcac2841c5afc71e9e2c52b529
aaccurso <accurso.alan@gmail.com>
2014-10-17 12:58:54 -0300

    adds ground prefab and configures collisions with the bird

---
+++ game/prefabs/ground.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Ground = function(game, x, y, width, height) {
+  Phaser.TileSprite.call(this, game, x, y, width, height, 'ground');
+
+  // start scrolling our ground
+  this.autoScroll(-200,0);
+
+  // enable physics on the ground sprite
+  // this is needed for collision detection
+  this.game.physics.arcade.enableBody(this);
+
+  // we don't want the ground's body
+  // to be affected by gravity nor collisions
+  this.body.allowGravity = false;
+  this.body.immovable = true;
+};
+
+Ground.prototype = Object.create(Phaser.TileSprite.prototype);
+Ground.prototype.constructor = Ground;
+
+Ground.prototype.update = function() {
+
+  // write your prefab's specific update code here
+
+};
+
+module.exports = Ground;
--- game/states/play.js
+++ game/states/play.js
@@ -1,6 +1,7 @@
 
   'use strict';
   var Bird = require('../prefabs/bird');
+  var Ground = require('../prefabs/ground');
 
   function Play() {}
   Play.prototype = {
@@ -15,8 +16,13 @@
       this.bird = new Bird(this.game, 100, this.game.height/2);
       // and add it to the game
       this.game.add.existing(this.bird);
+
+      // create and add a new Ground object
+      this.ground = new Ground(this.game, 0, 400, 335, 112);
+      this.game.add.existing(this.ground);
     },
     update: function() {
+      this.game.physics.arcade.collide(this.bird, this.ground);
     }
   };
 
