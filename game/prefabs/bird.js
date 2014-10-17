60145a736f5421edfc308d795c389456648e5f88
aaccurso <accurso.alan@gmail.com>
2014-10-17 13:10:03 -0300

    adds bird rotation

--- game/prefabs/bird.js
+++ game/prefabs/bird.js
@@ -19,13 +19,18 @@ Bird.prototype = Object.create(Phaser.Sprite.prototype);
 Bird.prototype.constructor = Bird;
 
 Bird.prototype.update = function() {
-
-  // write your prefab's specific update code here
-
+  // check to see if our angle is less than 90
+  // if it is rotate the bird towards the ground by 2.5 degrees
+  if(this.angle < 90) {
+    this.angle += 2.5;
+  }
 };
 
 Bird.prototype.flap = function() {
     this.body.velocity.y = -400;
+
+    // rotate the bird to -40 degrees
+    this.game.add.tween(this).to({angle: -40}, 100).start();
 };
 
 module.exports = Bird;
