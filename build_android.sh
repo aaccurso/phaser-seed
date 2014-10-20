668c24a5ca1580f232fee260b38ae8b83b297d86
aaccurso <accurso.alan@gmail.com>
2014-10-20 15:25:27 -0300

    moves x86 apk also

--- build_android.sh
+++ build_android.sh
@@ -5,5 +5,5 @@ BUILD_HOME=$PROJECT_HOME/build
 
 cd $DEV_HOME/crosswalk-8.37.189.12 &&
 python make_apk.py --package $1 --manifest $DIST_HOME/manifest.json &&
-rm -fr $BUILD_HOME/$2 && mv -f $2 $BUILD_HOME && mv -f $2_arm.apk $BUILD_HOME && rm -f $2_x86.apk
+rm -fr $BUILD_HOME/$2 && mv -f $2 $BUILD_HOME && mv -f $2_arm.apk $BUILD_HOME && mv -f $2_x86.apk $BUILD_HOME
 cd $PROJECT_HOME
