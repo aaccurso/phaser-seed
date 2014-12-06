a19246cd82d69749dc0994d0637c37ebac81f0fa
aaccurso <accurso.alan@gmail.com>
2014-12-06 00:29:28 -0300

    refactor(build): renames canvas wrapper and projectName to 'game'
    Conflicts:
    	config.json
    	game/main.js

--- game/main.js
+++ game/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.onload = function () {
-  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'flappybird');
+  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'game');
   
   game.state.add('boot', require('./states/boot'));
   game.state.add('menu', require('./states/menu'));
