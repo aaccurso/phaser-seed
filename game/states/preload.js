097ac03428cfaa62e5ba465abb3e402400a822d7
Hernan Gabriel Pieroni <hernan.pieroni@graion.com>
2014-12-31 13:10:29 -0300

    feat(boot, play, preload): Changes to  states for startup project

--- game/states/preload.js
+++ game/states/preload.js
@@ -1,48 +1,21 @@
-
 'use strict';
-function Preload() {
-  this.asset = null;
-  this.ready = false;
-}
+
+function Preload() {}
 
 Preload.prototype = {
+  init: function () {
+
+  },
   preload: function() {
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
-    this.asset = this.add.sprite(this.width / 2, this.height / 2, 'preloader');
-    this.asset.anchor.set(0.5);
-    this.load.setPreloadSprite(this.asset);
+    this.preloader = this.add.sprite(this.world.centerX, this.world.centerY, 'preloader');
+    this.preloader.anchor.set(0.5);
+    this.load.setPreloadSprite(this.preloader);
 
-    this.load.image('background', 'assets/background.png');
-    this.load.image('ground', 'assets/ground.png');
-    this.load.image('title', 'assets/title.png');
-    this.load.image('startButton', 'assets/start-button.png');
-    this.load.image('instructions', 'assets/instructions.png');
-    this.load.image('getReady', 'assets/get-ready.png');
-    this.load.image('scoreboard', 'assets/scoreboard.png');
-    this.load.image('gameover', 'assets/gameover.png');
-    this.load.image('particle', 'assets/particle.png');
-
-    this.load.spritesheet('medals', 'assets/medals.png', 44, 46, 2);
-    this.load.spritesheet('bird', 'assets/bird.png', 34, 24, 3);
-    this.load.spritesheet('pipe', 'assets/pipes.png', 54, 320, 2);
-
-    this.load.bitmapFont('flappyfont', 'fonts/flappyfont/flappyfont.png', 'fonts/flappyfont/flappyfont.fnt');
-
-    this.load.audio('score', 'assets/score.wav');
-    this.load.audio('flap', 'assets/flap.wav');
-    this.load.audio('pipeHit', 'assets/pipe-hit.wav');
-    this.load.audio('groundHit', 'assets/ground-hit.wav');
-  },
-  create: function() {
-    this.asset.cropEnabled = false;
-  },
-  update: function() {
-    if (!!this.ready) {
-      this.game.state.start('menu');
-    }
+    this.load.image('yeoman', 'assets/yeoman-logo.png');
   },
   onLoadComplete: function() {
-    this.ready = true;
+    this.state.start('play');
   }
 };
 
