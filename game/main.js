f144ca4064dca4912fde66db515548d3e84129bc
Hernan Gabriel Pieroni <hernan.pieroni@graion.com>
2014-12-31 12:33:35 -0300

    feat(): Deletes unnecesary code

--- game/main.js
+++ game/main.js
@@ -4,7 +4,6 @@ window.onload = function () {
   var game = new Phaser.Game(288, 505, Phaser.AUTO, 'game');
   
   game.state.add('boot', require('./states/boot'));
-  game.state.add('menu', require('./states/menu'));
   game.state.add('play', require('./states/play'));
   game.state.add('preload', require('./states/preload'));
 
