d4ee52c4ba46763485a7e8173c5664788f88cd25
Hernan Gabriel Pieroni <hernan.pieroni@graion.com>
2014-12-31 15:08:54 -0300

    feat(main.js, config.json): changes game resolution

--- game/main.js
+++ game/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.onload = function () {
-  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'game');
+  var game = new Phaser.Game(1280, 690, Phaser.AUTO, 'game');
   
   game.state.add('boot', require('./states/boot'));
   game.state.add('play', require('./states/play'));
